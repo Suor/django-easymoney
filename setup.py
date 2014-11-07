6be9dfe4521c128b9ff47ff9393644a2d79a3d8e
Alexander Schepanovski <suor.web@gmail.com>
2014-11-07 14:54:18 +0700

    Up to 0.4.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-easymoney',
-    version='0.4',
+    version='0.4.1',
     author='Alexander Schepanovski',
     author_email='suor.web@gmail.com',
 
