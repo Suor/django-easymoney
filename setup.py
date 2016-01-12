7335509eb20bcc1d871c58d66f1cc1e4be0b11bd
Gregor Müllegger <gregor@muellegger.de>
2016-01-12 15:31:10 +0100

    Add Python version trove classifiers

--- setup.py
+++ setup.py
@@ -27,6 +27,12 @@ setup(
         'Framework :: Django',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
