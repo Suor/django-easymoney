caa8191d6520b2f90b5f44fb9c629ea45a42f257
Alexander Schepanovski <suor.web@gmail.com>
2016-02-19 20:45:50 +0700

    Support Django 1.9

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         'Framework :: Django :: 1.6',
         'Framework :: Django :: 1.7',
         'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 2',
